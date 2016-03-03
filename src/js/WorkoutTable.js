261aaeb95f646bffc21b78a32f0411140d5c64f4
Kaj Ström <kaj.strom@kapsi.fi>
2016-03-03 18:24:21 +0200

    Workout table now uses the new models

--- src/js/WorkoutTable.js
+++ src/js/WorkoutTable.js
@@ -3,7 +3,18 @@ var Marionette = require("backbone.marionette");
 var WorkoutTable = Marionette.ItemView.extend({
     template: require("../templates/workouts-table-tpl.ejs"),
     tagName: "table",
-    className: "table"
+    className: "table",
+
+    serializeData() {
+        let attrs = this.model.attributes;
+
+        attrs.week1 = attrs.week1.toJSON();
+        attrs.week2 = attrs.week2.toJSON();
+        attrs.week3 = attrs.week3.toJSON();
+        attrs.week4 = attrs.week4.toJSON();
+
+        return attrs;
+    }
 });
 
 module.exports = WorkoutTable;
